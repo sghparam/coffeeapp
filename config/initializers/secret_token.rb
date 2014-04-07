d638077354d4a613d5aad60a65d8d9394c550684
unknown <Paramjeet.Singh@WS13591.VE.LOCAL>
2014-04-07 10:19:48 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Coffeeapp::Application.config.secret_key_base = 'ae30881228181f5c6821c84e170ef43c39701edfb31f928096b84148c786bad5f67cbafa366e7ce0ccb6cb4b8fe2ea1d3d2539f2e2f94729fc59c132e0a623e0'
